fa3f3a4b05e4785ab52b8690c8a24421d76eea12
Vignesh M <93856871+Vky08@users.noreply.github.com>
2022-11-12 15:05:19 +0530

    Delete Assingment directory

--- Assingment/Assingment 2/Assingment 2.py
+++
@@ -1,8 +0,0 @@
-import random
-while(True):
-    temp=random.randrange(0,100)
-    humid=random.randrange(0,100)
-    print("Temperature: ",temp," Humidity: ",humid)
-    if (temp>90 or temp<80) and humid>60:
-        print("------Dangerious!!!------")
-        break
